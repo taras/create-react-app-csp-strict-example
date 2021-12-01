48a4a27ed29a4334d515c33fdceb02831af9dfea
Paul Cowan <paul.cowan@cutting.scot>
2021-12-01 10:28:02 +0000

    more docker

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:14.17.5-alpine as build
+WORKDIR /app
+
+ENV PATH /app/node_modules/.bin:$PATH
+
+COPY package.json ./
+
+RUN npm cache clean --force
+
+RUN npm install --production
+
+RUN npm install --save-dev rewire
+
+COPY . ./
+
+RUN chmod +x ./scripts/*
+
+RUN npm run build
+
+FROM nginx:stable-alpine
+
+COPY --from=build /app/build /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD ["nginx", "-g", "daemon off;"]
---
+++ scripts/build.js
@@ -0,0 +1,15 @@
+const rewire = require('rewire');
+const defaults = rewire('react-scripts/scripts/build.js');
+const config = defaults.__get__('config');
+const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
+
+// TODO: if this does not work then try https://github.com/styled-components/styled-components/issues/887#issuecomment-376479268
+config.plugins.push(new ScriptExtHtmlWebpackPlugin({
+  custom: {
+    test: /\.js$/,
+    attribute: "nonce",
+    value: "{{ cspNonce }}"
+  }
+}))
+
+config.target = 'web';
