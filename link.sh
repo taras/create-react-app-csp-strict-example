8867c99c9033ab350b1e98e6bfdd373fa33ccbf7
Paul Cowan <paul.cowan@cutting.scot>
2021-12-22 13:16:35 +0000

    add link.sh and unlink.sh

---
+++ link.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yarn link "netlify-cms-core"
+yarn link "netlify-cms"
+yarn link "netlify-cms-lib-auth"
+yarn link "netlify-cms-lib-util"
+yarn link "netlify-cms-lib-widgets"
+yarn link "netlify-cms-locales"
+yarn link "netlify-cms-media-library-cloudinary"
+yarn link "netlify-cms-media-library-uploadcare"
---
+++ unlink.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yarn unlink "netlify-cms-core"
+yarn unlink "netlify-cms"
+yarn unlink "netlify-cms-lib-auth"
+yarn unlink "netlify-cms-lib-util"
+yarn unlink "netlify-cms-lib-widgets"
+yarn unlink "netlify-cms-locales"
+yarn unlink "netlify-cms-media-library-cloudinary"
+yarn unlink "netlify-cms-media-library-uploadcare"
